2a0a772f378e9286d3ecf8476ee71656d80fa1af
thomasjfcaldwell <thomasjfcaldwell@gmail.com>
2020-07-09 13:24:23 -0700

    half way

---
+++ READ.md
@@ -0,0 +1,17 @@
+
+# Tic Tac Toe
+A game in HTML, CSS and JS.
+## User Stories
+MVP
+- As a player, I want my game to recognize three ticks in a row so that I know who won.
+- As a player, I would like to be informed when the game is over due to a tie.
+- As a player, I would like to know whose turn it is so that I don't have to keep track.
+- As a player, I would like to be informed with some indication when I mark a game box so that I know my selection worked.
+- As a player who requires assistive technologies, I would like accessibility features so that I'm not left out of enjoying the game.
+- As a player I would like to be able to restart the game after a win, loss or tie.
+- As a player, I want the UI to be engaging and out of the way so that I enjoy the experience of playing the game.\*\*
+Stretch
+- As a player, I would like a victory animation when I win the game, so that I feel good about my victory!
+- As a player, I would like to hear an audible sound when I mark a game box so that I know my selection worked.
+- As a player, I would like to play this game online so that I don't have to have my challenger sitting next to me.
+- As a player who wants to dominate the battlefield I want to become the ultimate tic tac toe tactician so I can beat all of my opponents
---
+++ app.js
@@ -0,0 +1,64 @@
+
+//let gameboard = 
+
+/*function checkWinCondition() {
+	// checks for win conditions
+}
+
+function checkTieCondition() {
+	// check if there is a tie
+}
+
+function switchPlayers() {
+	// switch between "x" and 'o'
+	if (currentPlayer === 'X') {
+		currentPlayer = 'O';
+	} else {
+		currentPlayer = 'X';
+	}
+ currentPlayer = currentPlayer === 'X' ? "O" : "X";
+}
+
+function checkBoxNotOccupied() {
+	//determine if the game box that is clicked is empty!
+}
+
+function startGame() {}
+
+function turnSequence() {
+	// all the things that happen when a user takes a turn
+}
+
+function writeMessage(message) {
+	// update the gameboard message with
+}
+*/
+
+//When a user clicks on a square, mark the square with the current user's token (alternating with each click). You can add an X or an O or some other marker that corresponds to the current player.
+let currentPlayer = 'X';
+function switchPlayer() {
+currentPlayer = currentPlayer === 'X' ? 'O' : 'X';
+}
+
+let playBox = document.querySelectorAll(".box");
+//console.log(square);
+
+
+playBox.forEach((playBox)=>{
+    playBox.addEventListener( 'click',playerClickEvent)
+})
+
+
+function playerClickEvent(event) {
+    //console.log(event.target.id);
+    event.target.innerHTML = currentPlayer;
+switchPlayer()
+    
+}
+const reSetIt = document.querySelector('button')
+
+reSetIt.addEventListener('click',resetClickEvent)
+
+function resetClickEvent(event){
+    
+}
